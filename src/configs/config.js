36b8424e4e996490801c92de2bf4468c74094016
Gabriel Ferrin <111001078+GabrielFerrin@users.noreply.github.com>
2024-07-26 16:55:19 -0500

    Delete src/configs/config.js
    arreglar conflictos

--- src/configs/config.js
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-
-config()
-
-export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST
-export const allowedOrigins = ['http://localhost:5173']
